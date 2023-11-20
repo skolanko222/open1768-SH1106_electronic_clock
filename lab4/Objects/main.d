50b04f9900915e3fbafcc99f848ea592f1b7da33
skolanko222 <skolanko222@gmail.com>
2023-11-20 16:34:54 +0100

    lab4

---
+++ lab4/Objects/main.d
@@ -0,0 +1,15 @@
+.\objects\main.o: main.c
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Driver\Include\Driver_USART.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Driver\Include\Driver_Common.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.6.1\Device\Include\LPC17xx.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\core_cm3.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_version.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.5.1\CMSIS\Core\Include\mpu_armv7.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.6.1\Device\Include\system_LPC17xx.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.6.1\RTE_Driver\PIN_LPC17xx.h
---
+++ lab4/main.c
@@ -0,0 +1,85 @@
+#include "Driver_USART.h"
+#include "LPC17xx.h"                    // Device header
+#include "string.h" 
+#include "PIN_LPC17xx.h"
+
+void TIMER0_IRQHandler(void){
+		const char napis[] = "Ping";
+		for(int i = 0; i < sizeof(napis) ; i++){
+				
+				if(LPC_UART0->LSR >> 6 == 1){
+				LPC_UART0->THR = napis[i];
+				}
+				else{
+					i--;
+				}
+			}
+		LPC_TIM0->IR = 1;
+}
+void RTC_IRQHandler(void){
+	const char * n1 = "Tik\n";
+	const char * n2 = "Tak\n";
+	const char * napis;
+	static char flag = 0;
+	
+	if(flag == 0){
+		flag = 1;
+		napis = n1;
+	}
+	else if(flag == 1){
+		flag = 0;
+		napis = n2;
+	}
+	
+	for(int i = 0; i < sizeof(n1) ; i++){
+				
+				if(LPC_UART0->LSR >> 6 == 1){
+				LPC_UART0->THR = napis[i];
+				}
+				else{
+					i--;
+				}
+			}
+	
+	LPC_RTC->ILR = 1;
+
+}
+
+int main() {
+
+		SysTick_Config(SystemCoreClock / 100); //10ms
+	
+		PIN_Configure(0,2,1,2,0);
+		PIN_Configure(0,3,1,2,0);
+	
+
+		LPC_UART0->LCR = 3 | (1<<7);
+		LPC_UART0->DLL = 27; // dzielnik peryferyjny 2, 256 max
+		LPC_UART0->DLM = 0; // tu naddatek x*256
+		LPC_UART0->FCR = 6;
+		
+		
+		LPC_UART0->LCR = 3;
+	
+	
+		LPC_TIM0->PR = 0;
+		LPC_TIM0->MCR = 3;
+		LPC_TIM0->MR0 = 12500000;
+		LPC_TIM0->TCR = 1;
+		
+		//RTC
+		LPC_RTC->CCR = 1;
+		LPC_RTC->ILR = 1;
+		LPC_RTC->CIIR = 1;
+	
+		
+		NVIC_EnableIRQ(TIMER0_IRQn);
+		NVIC_EnableIRQ(RTC_IRQn);
+		
+		while(1)
+		{
+
+		}
+	
+	return 0;
+}
